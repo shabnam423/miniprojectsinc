a89a0f0fc00b19d8d7b83166617e40eb9daac2d8
shabnam423 <sebnemnezerli13@gmail.com>
2024-05-02 15:53:49 +0400

    added p4

--- project4.c
+++ project4.c
@@ -1,39 +1,78 @@
-//fix tryout.c then copy it here
-
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+
 int main()
 {
-
-    int attempts;
-    char word[20];
-    int found; // the elements that have been found
-
-    printf("Enter a word(max 20 elements)");
-    scanf("%s", &word);
-    found = 0;
-    attempts = strlen(word);
+    char word[21]; 
+    printf("Enter a word (max 20 letters): ");
+    scanf("%20s", word);
+    
     int length = strlen(word);
-    char foundletters[length];
-    while (found != strlen(word))
+    char arr[length];
+    int attempts = length+5;
+
+    for (int i = 0; i < length; i++)
     {
-        for (int i = 0; i < attempts; i++)
+        arr[i] = '-';
+    }
+
+    for (int j = 0; j < length; j++)
+    {
+        printf("%c ", arr[j]);
+    }
+printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    // Sozun uzunlugu sayda userden input telebi
+    for (int k = 0; k < length; k++)
+    {
+        char character;
+        printf("\nEnter a char: ");
+        scanf(" %c", &character);
+
+        attempts--;
+
+        bool found = false;
+        for (int l = 0; l < length; l++)
         {
-            char character;
-            printf("Enter a character: ");
-            scanf("%c", character);
-            attempts--;
-
-            if (character == word[i])
+            if (character == word[l])
             {
-
-                printf("Well done! You found a letter!\nAttemps left:%d", attempts);
-                word[i] = foundletters[i];
-
+                found = true;
+                arr[l] = character;
             }
+        }
+
+        if (found)
+        {
+            printf("CONGRATS! Found letter: %c\nYou have %d attempts left.\n", character, attempts);
+        }
+        else
+        {
+            printf("You failed, continue to guess.. You have %d attempts left.\n", attempts);
+        }
+
+        // Print current state of the word
+        for (int m = 0; m < length; m++)
+        {
+            printf("%c ", arr[m]);
+        }
+
+        // Check if the word has been completely guessed
+        bool allGuessed = true;
+        for (int n = 0; n < length; n++)
+        {
+            if (arr[n] == '-')
+            {
+                allGuessed = false;
+                break;
+            }
+        }
+
+        if (allGuessed)
+        {
+            printf("\nCongratulations! You guessed the word.\n");
+            break;
         }
     }
 
-
-return 0;
+    return 0;
 }
