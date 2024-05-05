6df6579d3f484d014c11c5d8d031ba57b8efc11a
shabnam423 <sebnemnezerli13@gmail.com>
2024-05-05 11:12:29 +0400

    last update for p6

--- project6.c
+++ project6.c
@@ -45,24 +45,7 @@ int months(int leap)
     return 0;
 }
 
-/*
-input olaraq daxil edilir 1999
-1999cu ilin yanvardan dekabra qeder kalendar cixacaq
 
-
-Enter a year: 2024
-Calendar for the year 2024:
-
----------------- January ----------------
-Sun Mon Tue Wed Thu Fri Sat
-   1   2   3   4   5   6
-7   8   9  10  11  12  13
-14  15  16  17  18  19  20
-21  22  23  24  25  26  27
-28  29  30  31
-
-
-*/
 int main()
 {
     printf("Input a year to show: ");
