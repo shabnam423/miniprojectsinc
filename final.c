06cbaec10d101b52619e49dd5c7c543b5feeccac
shabnam423 <sebnemnezerli13@gmail.com>
2024-05-08 09:10:24 +0400

    final& project7 added

--- final.c
+++ final.c
@@ -1,25 +1,35 @@
 // Features:
 // 1- Book Structure: Define a structure to represent books, including attributes like title,
-// author, genre, price, and quantity.
-// 2- Book Inventory: Create an array to store information about available books in the shop.
+// author, genre, price, and quantity.  ++
+// 2- Book Inventory: Create an array to store information about available books in the shop.  ++
 // 3- Add New Book: Implement functionality to add a new book to the inventory. Prompt the
-// user to input details such as title, author, genre, price, and quantity.
+// user to input details such as title, author, genre, price, and quantity.     ++
 // 4- Display All Books: Allow the user to display all books in the inventory, showing details
-// such as title, author, genre, price, and quantity.
+// such as title, author, genre, price, and quantity.   ++
 // 5- Search Books: Implement functionality to search for books by title, author, or genre.
-// Display the details of matching books.
+// Display the details of matching books.   ++
 // 6- Update Book Details: Allow the user to update the details of existing books, such as
-// price and quantity.
+// price and quantity.  ++
 // 7- Sell Books: Implement functionality to sell books to customers. Prompt the user to
 // select a book from the inventory and specify the quantity to sell. Update the quantity of
-// the book in the inventory accordingly.
+// the book in the inventory accordingly.   ++
 // 8- View Sales Report: Provide an option to view a sales report, showing total sales revenue
 // and the quantity of each book sold.
 // 9- Save and Load Data: Add functionality to save the book inventory and sales data to a
 // file, allowing users to persist data between sessions.
-
 #include <stdio.h>
+#include <string.h>
+#define Red "\x1b[31m"
+#define Yellow "\x1b[33m"
+#define Blue "\x1b[34m"
+#define Magenta "\x1b[35m"
+#define Cyan "\x1b[36m"
+#define Gray "\x1b[90m"
+#define Light_Red "\x1b[91m"
+#define Green "\x1b[32m"
+#define Reset "\x1b[0m"
 
+#define MAX_BOOKS 50
 struct Book
 {
     char title[50];
@@ -28,8 +38,203 @@ struct Book
     float price;
     int quantity;
 };
-int main()
+int UpdateBookDetails(struct Book bookArray[])
+{
+    int choice;
+    int i;
+
+    printf("Enter the index of the book you want to update: ");
+    scanf("%d", &i);
+
+    if (i < 0 || i >= MAX_BOOKS)
+    {
+        printf("Invalid book index.\n");
+        return 0;
+    }
+    if (strcmp(bookArray[i].title, "") == 0)
+    {
+        printf("Book not found.\n");
+        return 0;
+    }
+    printf("Enter 1 to update price, 2 to update quantity: ");
+    scanf("%d", &choice);
+
+    if (choice == 1)
+    {
+        printf("Enter the new price: ");
+        scanf("%f", &bookArray[i].price);
+    }
+    else if (choice == 2)
+    {
+        printf("Enter the new quantity: ");
+        scanf("%d", &bookArray[i].quantity);
+    }
+    else
+    {
+        printf("Invalid choice.\n");
+        return 0;
+    }
+}
+
+int List(int choice)
 {
 
+    printf("Choose an option: \n");
+    printf("1. Display all Books: \n");
+    printf("2. Add New Book \n");
+    printf("3. Search Books: \n");
+    printf("4. Update Book Details \n"); // only price & quantity
+    printf("5. Sell Books \n");
+    printf("6. View Sales Report \n");
+    printf("7. Save and Load Data \n");
+    printf(Red "8. Exit \n" Reset);
+    scanf("%d", &choice);
+    return choice;
+}
+void ShowBooks(struct Book bookArray[])
+{
+    for (int i = 0; i < MAX_BOOKS; i++)
+    {
+        printf("\"%s\" by %s, Genre: %s, Price: $%.2f, Quantity: %d\n", bookArray[i].title, bookArray[i].author, bookArray[i].genre, bookArray[i].price, bookArray[i].quantity);
+    }
+}
+
+int main()
+{
+struct Book soldBook[MAX_BOOKS];
+    struct Book bookArray[MAX_BOOKS];
+int soldBookindex=0;
+    strcpy(bookArray[0].title, "The Great Gatsby");
+    strcpy(bookArray[0].author, "F. Scott Fitzgerald");
+    strcpy(bookArray[0].genre, "Fiction");
+    bookArray[0].price = 10.99;
+    bookArray[0].quantity = 5;
+
+    strcpy(bookArray[1].title, "To Kill a Mockingbird");
+    strcpy(bookArray[1].author, "Harper Lee");
+    strcpy(bookArray[1].genre, "Fiction");
+    bookArray[1].price = 9.99;
+    bookArray[1].quantity = 3;
+
+    strcpy(bookArray[2].title, "Pride and Prejudice");
+    strcpy(bookArray[2].author, "Jane Austen");
+    strcpy(bookArray[2].genre, "Fiction");
+    bookArray[2].price = 7.99;
+    bookArray[2].quantity = 2;
+
+    strcpy(bookArray[3].title, "The Catcher in the Rye");
+    strcpy(bookArray[3].author, "J.D. Salinger");
+    strcpy(bookArray[3].genre, "Fiction");
+    bookArray[3].price = 8.99;
+    bookArray[3].quantity = 1;
+while(1){
+    
+    int choice;
+    int List(choice);
+    if (choice == 1)
+    {
+        ShowBooks(bookArray);
+    }
+    else if (choice == 2)
+    {
+        int i;
+        printf("Enter the number of books you want to add: ");
+        scanf("%d", &i);
+        if (i > 45)
+        {
+            printf("Book not added. Please enter a valid number of books. \n");
+            scanf("%d", &i);
+        }
+        for (int j = 0; j < i; j++)
+        {
+            printf("Enter the details of book %d:\n", j + 1);
+            printf("Title: ");
+            scanf("%s", bookArray[j].title);
+            printf("Author: ");
+            scanf("%s", bookArray[j].author);
+            printf("Genre: ");
+            scanf("%s", bookArray[j].genre);
+            printf("Price: ");
+            scanf("%f", &bookArray[j].price);
+            printf("Quantity: ");
+            scanf("%d", &bookArray[j].quantity);
+            printf("\n");
+        }
+    }
+    else if (choice == 3)
+    {
+        printf("Enter 1 to search by title, 2 to search by author, 3 to search by genre: ");
+        scanf("%d", &choice);
+        if (choice == 1)
+        {
+            printf("Enter the title of the book you want to search: ");
+            char title[50];
+            scanf("%s", title);
+            for (int i = 0; i < MAX_BOOKS; i++)
+            {
+                if (strcmp(title, bookArray[i].title) == 0)
+                {
+                    printf("\"%s\" by %s, Genre: %s, Price: $%.2f, Quantity: %d\n", bookArray[i].title, bookArray[i].author, bookArray[i].genre, bookArray[i].price, bookArray[i].quantity);
+                }
+            }
+        }
+        else if (choice == 2)
+        {
+            printf("Enter the author of the book you want to search: ");
+            char author[50];
+            scanf("%s", author);
+            for (int i = 0; i < MAX_BOOKS; i++)
+            {
+                if (strcmp(author, bookArray[i].author) == 0)
+                {
+                    printf("\"%s\" by %s, Genre: %s, Price: $%.2f, Quantity: %d\n", bookArray[i].title, bookArray[i].author, bookArray[i].genre, bookArray[i].price, bookArray[i].quantity);
+                }
+            }
+        }
+        else if (choice == 3)
+        {
+            printf("Enter the genre of the book you want to search: ");
+            char genre[50];
+            scanf("%s", genre);
+            for (int i = 0; i < MAX_BOOKS; i++)
+            {
+                if (strcmp(genre, bookArray[i].genre) == 0)
+                {
+                    printf("\"%s\" by %s, Genre: %s, Price: $%.2f, Quantity: %d\n", bookArray[i].title, bookArray[i].author, bookArray[i].genre, bookArray[i].price, bookArray[i].quantity);
+                }
+            }
+        }
+        else
+        {
+            printf("Invalid choice. Please try again and enter a valid choice: \n");
+            scanf("%d", &choice);
+        }
+    }
+    else if (choice == 4)
+    {
+        UpdateBookDetails(bookArray);
+    }
+    else if (choice == 5)
+    {
+        ShowBooks(bookArray);
+        int i;
+
+        printf("Select the index of the book you want to sell: ");
+        scanf("%d", &i);
+        if (i < 0 || i >= MAX_BOOKS)
+        {
+            printf("Invalid book index.\n");
+            printf("Try again...\nSelect the index of the book you want to sell: ");
+            scanf("%d", &i);
+            //kitabin indexin goturduk-indi sat-yeni sil o kitabi.
+        }
+        //if choice==6:
+        /*soldBook arrayini for icine aliriq ve ve ordaki melumatlari file icine yaziriq*/
+        
+       soldBook[soldBookindex]=bookArray[i];
+soldBookindex++;
+    }
+    ShowBooks(bookArray);
+}
     return 0;
 }
---
+++ project7.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+register
+1.login
+2.qeydiyyat
+3.exit
+
+2 secilibse ad gmail ve password goturulur. 
+qeyd! gmail evvelden var idise- evvelden qeydiyyatdan kecilibse o mail ile siz artiq qeydiyyatdan kecibsiniz yazilacaq.
+yoxdursa, qeydiyyatdan kecdiniz yazilsin
+
+
+1 secilibse gmail ve password goturulur.
+eger evvelden qeydiyyatdan kecilibse siz hesaba daxil oldunuz.
+yoxdusa da 'qeydiyyatdan kecin' yazilsin
+
+*/
+int main(){
+
+    return 0;
+}
